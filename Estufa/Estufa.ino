0403c994c0ac7953700077c15a11583ca4ca677e
Gabriel Rodriguez <gdlm.gdlm@hotmail.com>
2021-05-28 18:31:51 -0300

    P2

---
+++ Estufa/Estufa.ino
@@ -0,0 +1,194 @@
+//Mapenado pinos do Arduino
+int pino_cooler = 13;
+int pino_rele1 = 12;
+int pino_rele2 = 11;
+int pino_termometro = A0;
+int pino_umidade = A1;
+
+//configurando variaveis 
+int umidade_limite = 0;
+int temperatura_limite = 0;
+int timer = 0;
+int timer_agua = 0;
+bool ligar = false;
+
+//Incluindo bibliotecas
+#include <PubSubClient.h>
+
+#define TOPICO_SUBSCRIBE "EstufaInteligenteSend"
+#define TOPICO_PUBLISH   "EstufaInteligenteRecive"
+
+#define ID_MQTT  "Estufainteligente05K"  
+
+
+// Cinfiguração do wifi
+const char* SSID = "Caçador107";
+const char* PASSWORD = "74240654";
+  
+//Configurando Broker
+const char* BROKER_MQTT = "iot.eclipse.org";
+int BROKER_PORT = 1883;
+ 
+ 
+//Variáveis e objetos globais
+WiFiClient espClient;
+PubSubClient MQTT(espClient);
+char EstadoDeSaida = '0';
+  
+//Prototypes
+void reconectWiFi(); 
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void VerificaConexoesWiFIEMQTT(void);
+void InitOutput(void);
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  reconectWiFi();
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+  MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+  
+  pinMode(pino_cooler, OUTPUT);
+  pinMode(pino_rele1, OUTPUT);
+  pinMode(pino_rele2, OUTPUT);
+  pinMode(pino_termometro, INPUT);
+  pinMode(pino_umidade, INPUT);
+}
+
+void loop() {
+  if(timer > 9000){
+    ligar = true;
+  }
+  else{
+    ligar = false;
+  }
+  
+  if(ligar){
+    digitalWrite(pino_rele2, LOW);
+  }
+  else{
+    digitalWrite(pino_rele2, HIGH);
+  }
+  timer = ( timer + 1 ) % 10000;
+
+
+  
+  int SensorTempTensao=analogRead(pino_termometro);
+  float Tensao = SensorTempTensao*5;
+  Tensao/=1023;
+
+  float TemperaturaC = Tensao / 0.010;
+
+  if(TemperaturaC > 20.00){
+    digitalWrite(pino_cooler, HIGH);
+  }
+  else{
+    digitalWrite(pino_cooler, LOW);
+  }
+
+
+
+  int Umiade=analogRead(pino_umidade);
+  if(Umiade > 306){
+    timer_agua = timer_agua + 1;
+    if(timer_agua < 2000){
+      digitalWrite(pino_rele1, LOW);
+    }
+    else{
+      timer = 0;
+      digitalWrite(pino_rele1, HIGH);
+    }
+  }
+  else{
+    timer_agua = 0;
+    digitalWrite(pino_rele1, HIGH);
+  }
+
+  VerificaConexoesWiFIEMQTT();
+  EnviaEstadoOutputMQTT();
+  MQTT.loop();
+  Serial.print("Umidade");
+  Serial.println(Umiade);
+  Serial.print("Temperatura");
+  Serial.println(TemperaturaC);
+  
+  
+  delay(100);
+}
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) 
+{
+    String msg;
+    
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+    if (msg.equals("L"))
+    {
+        digitalWrite(D0, LOW);
+        EstadoDeSaida = '1';
+    }
+ 
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+    if (msg.equals("D"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoDeSaida = '0';
+    }
+     
+}
+  
+
+void reconnectMQTT() 
+{
+    while (!MQTT.connected()) 
+    {
+        if (MQTT.connect(ID_MQTT)) 
+        {
+            Serial.println("Conectado ao MQTT!");
+            MQTT.subscribe(TOPICO_SUBSCRIBE); 
+        } 
+        else
+        {
+            delay(2000);
+        }
+    }
+}
+
+void reconectWiFi() 
+{
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+         
+    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+     
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(100)
+    }
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.println(WiFi.localIP());
+}
+
+void VerificaConexoesWiFIEMQTT(void)
+{
+    if (!MQTT.connected()) 
+        reconnectMQTT();
+     reconectWiFi();
+}
+ 
+void EnviaEstadoOutputMQTT(void)
+{
+    if (EstadoDeSaida == '0')
+      MQTT.publish(TOPICO_PUBLISH, "D");
+ 
+    if (EstadoDeSaida == '1')
+      MQTT.publish(TOPICO_PUBLISH, "L");
+      
+    delay(1000);
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # EstufaInteligente
 Projeto de Objetos Inteligentes Conectados
+
+Nosso projeto consistem em um ambiente controlado para que você possa dar uma melhor estimativa de vida para sua planta.
+Assim monitoramos algumas variavveis como temperatura e umidade do vaso para manter sua planta sempre nas melhores condições.
+
+Ligados ao arduino temos dois sensores:
+
+    . Sensor de umidade 
+
+    . Sensor de temperatura lm35
+
+Os dois são responsaveis por fornecer as informaçoes que o arduino precisa para controlar seus atuadores.
+
+
+Dessa forma podes interferir no comportamento dos seguintes atuadores:
+
+    . Lampada led
+
+    . Bomba de aquario
+
+    . Cooler 12V
+
+A lampada e o cooler servem para controlarmos a temperatura, dessa forma mantendo na temperatura ideal para a planta que você possuir.
+A lampada também tem a funcao de oferecer a quantidade de luz correta para o bom desenvolviemnto de sua planta.
+
+
+Enquanto isso a bomba de aquario joga a agua de um reservatório para o vaso de sua plantinha.
+
+
+O projeto consiste no monitoramento dos dois pinos dos sensores.
+Com essas informaçoes coletadas e mapeadas das entradas analogicas, realizamos algumas comparacoes com os valores escolhidos para sua planta e assim o arduino decide se ira ligar algum atuador correspondete ou desliga-lo.
